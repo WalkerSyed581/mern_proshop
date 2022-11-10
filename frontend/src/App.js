d9a8e2f3d153eb05657f4698ed702f96615a936a
WalkerSyed581 <sajeelh581@gmail.com>
2022-11-10 12:04:19 +0500

    Added ratings and products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
-
-
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import { Container }  from 'react-bootstrap'
+import HomeScreen from "./screens/HomeScreen"
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to ProShop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </>
   )
 }
