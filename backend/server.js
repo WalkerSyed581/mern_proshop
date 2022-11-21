f43516d56b61d07e174602b757ec7bf85fa01a59
WalkerSyed581 <sajeelh581@gmail.com>
2022-11-21 12:54:28 +0500

    Added basic auth

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from  'dotenv'
 import connectDB from'./config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import {notFound,errorHandler} from './middleware/errorMiddleware.js'
 
 dotenv.config()
@@ -11,13 +12,15 @@ connectDB()
 
 const app = express()
 
-
+app.use(express.json())
 
 app.get('/',(req,res) => {
 	res.send('API is running...')
 })
 
 app.use('/api/products',productRoutes)
+app.use('/api/user',userRoutes)
+
 
 app.use(notFound)
 app.use(errorHandler)
